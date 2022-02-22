39109698a9e9c17c6624238a2e06ab447c90549c
Omar Hesham <oommaarr.220022@gmail.com>
2022-02-22 05:24:08 +0200

    Prepare a route to send the current id

--- nfc-reader-webserver/nfc-reader-webserver.ino
+++ nfc-reader-webserver/nfc-reader-webserver.ino
@@ -2,48 +2,65 @@
 #include <ESP8266WebServer.h>
 
 
-/* Set the WiFi SSID & Password */
+/*** Set the WiFi SSID & Password ***/
 const char* ssid = "";  
 const char* password = ""; 
 
-// Set a server listen to port 80
+/*** Set a server listen to port 80 ***/
 ESP8266WebServer server(80);
 
+
+/***    Global Variables    ***/
+String id = "";
 
 
 void setup() {
   Serial.begin(115200);
   
-  Serial.printf("Connecting to %s ", ssid);
+  connectToWiFi(ssid, password);
+  
+  // Print ESP8266 Local IP Address
+  Serial.print("Local IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(100);
+  
+  server.on("/", handleOnConnect);
+  server.on("/getCurrentID", sendCurrentID);
+  server.onNotFound(handleNotFound);
+  
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+
+void loop() {
+  // Start the server listening
+  server.handleClient();
+}
+
+
+void connectToWiFi(const char* ssid, const char* password) {
+  Serial.printf("\nConnecting to %s ", ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
   }
-  Serial.println(" connected");
-
-  // Print ESP8266 Local IP Address
-  Serial.print("Local IP address: ");
-  Serial.println(WiFi.localIP());
-  delay(100);
-  
-  server.on("/", handle_OnConnect);
-  server.onNotFound(handle_NotFound);
-  
-  server.begin();
-  Serial.println("HTTP server started");
-}
-void loop() {
-
-  // Start the server listening
-  server.handleClient();
+  Serial.println("*************CONNECTED*************");
 }
 
-void handle_OnConnect() {
-  server.send(200, "text/html", "Connected"); 
+
+void handleOnConnect() {
+  server.send(200, "text/plain", "Connected"); 
 }
 
-void handle_NotFound(){
+
+void handleNotFound(){
   server.send(404, "text/plain", "Not found");
+}
+
+
+void sendCurrentID() {
+  server.send(200, "text/plain", id);
 }
