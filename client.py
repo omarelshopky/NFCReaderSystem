59bbba2f0eed1f03a1b1eab707a1d93093b6f6ae
Omar Hesham <oommaarr.220022@gmail.com>
2022-02-22 09:13:34 +0200

    Append id to google sheet

--- client.py
+++ client.py
@@ -1,24 +1,82 @@
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 import requests
 import time
 
 
+
 NODEMCU_IP = '192.168.1.145'
 GET_ID_ROUTE = '/getCurrentID'
+SPREADSHEET_ID = '13WlgxBmsFN52TfJ84lMdOJiY5h_mu4NoVKvLo8De1GQ' # The spreadsheet ID 
+SHEET_NAME = 'sheet1'
+HEADER_RANGE = 'A1:E1' # FIRST_COLUMN:LAST_COLUMN
+SERVICE_ACCOUNT_FILE = './service_key.json'
 
 
-ids = []
 
-def getCurrentID():
-    res = requests.get(f'http://{NODEMCU_IP}{GET_ID_ROUTE}')
-    return res.text
+class NFCReader():
+    ids = []
 
 
-def addToQueue(id):
-    if not id in ids and id != '':
-        ids.append(id)
+    def getID(self):
+        self.addToQueue(self.getCurrentId())
 
 
-while True:
-    addToQueue(getCurrentID())
-    print(ids)
-    time.sleep(0.2)
+    def getCurrentId(self):
+        res = requests.get(f'http://{NODEMCU_IP}{GET_ID_ROUTE}')
+        return res.text
+
+
+    def addToQueue(self, id):
+        if not id in self.ids and id != '':
+            self.ids.append(id)
+
+
+
+class SheetWriter:
+
+    def __init__(self):
+        creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE,
+                                                 scopes=['https://www.googleapis.com/auth/spreadsheets'])
+
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        self.sheet = service.spreadsheets()
+
+    
+    def append(self, id):
+        try:
+            res = self.sheet.values().append(
+                spreadsheetId=SPREADSHEET_ID,
+                range=f"{SHEET_NAME}!{HEADER_RANGE}",
+                valueInputOption="USER_ENTERED",
+                insertDataOption="INSERT_ROWS",
+                body={"values" : [[id]]}
+            ).execute()
+
+            # print(res)
+        
+        except HttpError as err:
+            print(err)
+
+
+
+def main():
+    nfcReader = NFCReader()
+    sheetWriter = SheetWriter()
+
+    
+    while True:
+        nfcReader.getID()
+        print(nfcReader.ids)
+
+        if len(nfcReader.ids) > 0:
+            sheetWriter.append(nfcReader.ids.pop(0))
+
+        time.sleep(0.5)
+
+    
+if __name__ == '__main__':
+    main()
